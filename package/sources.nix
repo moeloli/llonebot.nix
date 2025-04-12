f0ec4bea442a8d5b4be11b83e0f8c996c25faae9
initialencounter <2911583893@qq.com>
2025-04-12 10:52:33 +0800

    llonebot: v4.6.5 -> v4.8.0
    Signed-off-by: initialencounter <2911583893@qq.com>

--- package/sources.nix
+++ package/sources.nix
@@ -1,6 +1,6 @@
 { ... }: {
   # Updated by ./update.sh - do not update manually!
-  # Last updated: 2025-01-14.
+  # Last updated: 2025-04-12.
 
   # ./update.sh qq 3.2.13_241121
   qq_amd64_url = "https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.15_250110_amd64_01.deb";
@@ -11,8 +11,8 @@
   LiteLoaderUrl = "https://github.com/LiteLoaderQQNT/LiteLoaderQQNT/releases/download/1.2.3/LiteLoaderQQNT.zip";
   LiteLoaderHash = "sha256-xyI6cJuVFMHb/RHoka99GiB25T+IjUMUkJdfFw8PiBk=";
   # ./update.sh llonebot 4.4.1
-  LLOneBotUrl = "https://github.com/LLOneBot/LLOneBot/releases/download/v4.6.5/LLOneBot.zip";
-  LLOneBotHash = "sha256-+UoAgCSGIBNs5Di5UV+1QlK+zdaGZcqu+YBL8nI7Jfc=";
+  LLOneBotUrl = "https://github.com/LLOneBot/LLOneBot/releases/download/v4.8.0/LLOneBot.zip";
+  LLOneBotHash = "sha256-zqmU6GGJgpfy4gIt940swIdnVaHXNK6ZrUjSSuE9tzY=";
   # ./update.sh whale 0.1.2
   WhaleUrl = "https://github.com/initialencounter/whale/releases/download/v0.1.2/whale.zip";
   WhaleHash = "sha256-152u+Gm7k2ql69Rq54zQ6DnM8nSbrXd4xWomV7SsDkM=";
