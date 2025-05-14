1cbc59157f249b8a1254d71a09daa29bbf9ce483
initialencounter <2911583893@qq.com>
2025-05-14 19:46:47 +0800

    qq: 3.2.16_250401 -> 3.2.17_250429
    Signed-off-by: initialencounter <2911583893@qq.com>

--- package/sources.nix
+++ package/sources.nix
@@ -1,13 +1,13 @@
 { ... }: {
   # Updated by ./update.sh - do not update manually!
-  # Last updated: 2025-04-12.
+  # Last updated: 2025-05-14.
 
   # ./update.sh qq 3.2.13_241121
-  qq_version = "3.2.16_250401";
-  qq_amd64_url = "https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.16_250401_amd64_01.deb";
-  qq_amd64_hash = "sha256-f+7bo4zVSywnAE1WJPUjVLyFJQYlz56QVEah2uDSzP8=";
-  qq_arm64_url = "https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.16_250401_arm64_01.deb";
-  qq_arm64_hash = "sha256-av1Pu5ZXASc0Jmm0c7xxtmECIHAYWOo1NQOdI3fDmZA=";
+  qq_version = "3.2.17_250429";
+  qq_amd64_url = "https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.17_250429_amd64_01.deb";
+  qq_amd64_hash = "sha256-DDqLHl8Ig7miZTheRltBq+riLXyGGnW8NtcOI0PgKtc=";
+  qq_arm64_url = "https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.17_250429_arm64_01.deb";
+  qq_arm64_hash = "sha256-BxyJ9NNdMck3qk8wzCd07x/W9QlTMemHi3l0TIQ04ZY=";
   # ./update.sh liteloader 1.2.3
   LiteLoaderUrl = "https://github.com/LiteLoaderQQNT/LiteLoaderQQNT/releases/download/1.2.3/LiteLoaderQQNT.zip";
   LiteLoaderHash = "sha256-xyI6cJuVFMHb/RHoka99GiB25T+IjUMUkJdfFw8PiBk=";
