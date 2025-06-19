ddafb9c2a2851c08250af83a664da200b8a5f145
initialencounter <2911583893@qq.com>
2025-06-19 20:08:04 +0800

    fix: pmhq_hash

--- package/sources.nix
+++ package/sources.nix
@@ -1,13 +1,13 @@
 { ... }: {
   # Updated by ./update.sh - do not update manually!
-  # Last updated: 2025-06-15.
+  # Last updated: 2025-06-19.
 
   # ./update.sh pmhq 2.0.0
   pmhq_version = "2.0.1";
   pmhq_amd64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.0.1/pmhq-linux-x64.zip";
-  pmhq_amd64_hash = "sha256-JNbV4nvVNDMLMEd/VrKctvAqpxu53PMIprIxEE2Ghb4=";
+  pmhq_amd64_hash = "sha256-4yqKo7D7sq/whGEgEFDjF+h4l/5Hzm8aYvKHfO9N9K8=";
   pmhq_arm64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.0.1/pmhq-linux-arm64.zip";
-  pmhq_arm64_hash = "sha256-GBHnfgVFhjXRD7CYtteuQF4HwZA2lAEFRc4rQlmjFbA=";
+  pmhq_arm64_hash = "sha256-ibzO/UlH5TKPq2ZXLWTx0tE5mWQWO02DMnIsPgZe3kI=";
   # ./update.sh qq 3.2.13_241121
   qq_version = "3.2.17_250429";
   qq_amd64_url = "https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.17_250429_amd64_01.deb";
