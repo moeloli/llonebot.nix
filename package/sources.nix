9188e45a9b9a08bbea25c25dcc1097b070128021
initialencounter <2911583893@qq.com>
2025-07-05 17:12:25 +0800

    Revert: pmhq 2.1.0 -> 2.0.3

--- package/sources.nix
+++ package/sources.nix
@@ -3,11 +3,11 @@
   # Last updated: 2025-07-04.
 
   # ./update.sh pmhq 2.0.0
-  pmhq_version = "2.1.0";
-  pmhq_amd64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.1.0/pmhq-linux-x64.zip";
-  pmhq_amd64_hash = "sha256-3+RYpzau/zFVCypLS1FvfiStf/vB3yba96FNbxjUoMI=";
-  pmhq_arm64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.1.0/pmhq-linux-arm64.zip";
-  pmhq_arm64_hash = "sha256-CElWekmaNDYhOWF6xtrCa8W+do0Wd2mtV/XwlbJlL6A=";
+  pmhq_version = "2.0.3";
+  pmhq_amd64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.0.3/pmhq-linux-x64.zip";
+  pmhq_amd64_hash = "sha256-LszocLPaC6Ql+q9YpcbEXg2NLaVC3V8vjoiEtmgPFSY=";
+  pmhq_arm64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.0.3/pmhq-linux-arm64.zip";
+  pmhq_arm64_hash = "sha256-ZBJsYBIacKd6q//Vj0NoThZymJWKqSt06fBp+Q1lcys=";
   # ./update.sh qq 3.2.13_241121
   qq_version = "3.2.17_250429";
   qq_amd64_url = "https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.17_250429_amd64_01.deb";
