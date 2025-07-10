c53203ae8e63bf801064650d808019b550f60d91
initialencounter <2911583893@qq.com>
2025-07-10 17:09:12 +0800

    pmhq 2.1.1 -> 2.2.0

--- package/sources.nix
+++ package/sources.nix
@@ -4,11 +4,11 @@
   # Last updated: 2025-07-10.
 
   # ./update.sh pmhq 2.0.0
-  pmhq_version = "2.1.1";
-  pmhq_amd64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.1.1/pmhq-linux-x64.zip";
-  pmhq_amd64_hash = "sha256-nzX2FrY52JD1mzh5GLAteZqo6reJ3pwp1BSR+zb2+9Q=";
-  pmhq_arm64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.1.1/pmhq-linux-arm64.zip";
-  pmhq_arm64_hash = "sha256-I30JGaCiuPS6YOB84C54MUY2f25A6H2fCiRCoSnZh1U=";
+  pmhq_version = "2.2.0";
+  pmhq_amd64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.2.0/pmhq-linux-x64.zip";
+  pmhq_amd64_hash = "sha256-P+wVxb+kuhlUgoVzLK69kLFvivQBl8w8YtoUjQyOGlY=";
+  pmhq_arm64_url = "https://github.com/linyuchen/PMHQ/releases/download/v2.2.0/pmhq-linux-arm64.zip";
+  pmhq_arm64_hash = "sha256-qnY5WDj3+XQ0yYG0kUsvCtEyr4wTC4umpbQ5A4ADht0=";
   # ./update.sh llonebot 4.4.1
   llonebot_version = "5.4.0";
   llonebot_url = "https://github.com/LLOneBot/LLOneBot/releases/download/v5.4.0/LLOneBot.zip";
