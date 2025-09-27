d169497b061b605f147fcf0778919d2ea2ab50cd
initialencounter <2911583893@qq.com>
2025-09-27 16:38:48 +0800

    llonebot 6.1.1 -> 6.2.0

--- package/sources.nix
+++ package/sources.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   # Updated by ./update.sh - do not update manually!
-  # Last updated: 2025-09-23.
+  # Last updated: 2025-09-27.
 
   # ./update.sh pmhq 2.0.0
   pmhq_version = "4.1.1";
@@ -10,7 +10,7 @@
   pmhq_arm64_url = "https://github.com/linyuchen/PMHQ/releases/download/v4.1.1/pmhq-linux-arm64.zip";
   pmhq_arm64_hash = "sha256-3phnLUxtw1r8AveLV5DBBWlOtrtSrgciWLxO4qBclao=";
   # ./update.sh llonebot 4.4.1
-  llonebot_version = "6.1.1";
-  llonebot_url = "https://github.com/LLOneBot/LLOneBot/releases/download/v6.1.1/LLOneBot.zip";
-  llonebot_hash = "sha256-6BcMVOkcy5bjnhmwfKUOJJTpIzZCP1sPr9glwKZGfkg=";
+  llonebot_version = "6.2.0";
+  llonebot_url = "https://github.com/LLOneBot/LLOneBot/releases/download/v6.2.0/LLOneBot.zip";
+  llonebot_hash = "sha256-3C+EUFX3N0mmctviRMdtu0Qfr5v7tjtQ0Cx+HhYS12o=";
 }
