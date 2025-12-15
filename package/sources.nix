06513da25dca15d69b9426347e3a02b0d3944419
initialencounter <2911583893@qq.com>
2025-12-15 20:47:08 +0800

    fix: ci
    Signed-off-by: initialencounter <2911583893@qq.com>

--- package/sources.nix
+++ package/sources.nix
@@ -10,7 +10,7 @@
   pmhq_arm64_url = "https://github.com/linyuchen/PMHQ/releases/download/v5.2.2/pmhq-linux-arm64.zip";
   pmhq_arm64_hash = "sha256-sdLbtSuiQIQaSkevF3bB5yzuySn2s707tqIKPHP7UMs=";
   # ./update.sh llonebot 4.4.1
-  llonebot_version = "ull";
-  llonebot_url = "https://github.com/LLOneBot/LLOneBot/releases/download/vull/LLOneBot.zip";
-  llonebot_hash = "";
+  llonebot_version = "7.1.3";
+  llonebot_url = "https://github.com/LLOneBot/LLOneBot/releases/download/v7.1.3/LLOneBot.zip";
+  llonebot_hash = "sha256-CUoqjsmcuGc6I6evf27+qhq/k7vddyf3xRBJJVlteBg=";
 }
